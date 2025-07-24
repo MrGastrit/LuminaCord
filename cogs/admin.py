65c0f7f513233095b7bcd4d943f8e1c41609790b
MrGastrit <korpusovartem07@gmail.com>
2025-07-24 14:55:02 +0300

    music player, mute and emergency command

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
+import datetime
 
 class AdminCog(commands.Cog):
     def __init__(self, bot):
@@ -11,7 +12,7 @@ class AdminCog(commands.Cog):
     async def purge(self, ctx, amount: int):
         await ctx.channel.purge(limit=amount)
 
-    @app_commands.command(name="kick")
+    @app_commands.command(name="kick", description="Выгнать участника с сервера")
     @app_commands.describe(member="Пользователь", reason="Причина")
     @app_commands.checks.has_permissions(kick_members=True)
     async def kick(self, interaction: discord.Interaction, member: discord.Member, reason: str="Не указана"):
@@ -26,7 +27,7 @@ class AdminCog(commands.Cog):
         except Exception as e:
             print(e)
 
-    @app_commands.command(name="ban")
+    @app_commands.command(name="ban", description="Забанить участника сервера")
     @app_commands.describe(member="Пользователь", reason="Причина")
     @app_commands.checks.has_permissions(ban_members=True)
     async def ban(self, interaction: discord.Interaction, member: discord.Member, reason: str="Не указана"):
@@ -41,7 +42,8 @@ class AdminCog(commands.Cog):
         except Exception as e:
             print(e)
 
-    @app_commands.command(name="unban")
+    @app_commands.command(name="unban", description="Разбанить пользователя"
+                                                    "")
     @app_commands.describe(user="ID/Имя")
     @app_commands.checks.has_permissions(ban_members=True)
     async def unban(self, interaction: discord.Interaction, user: str, reason: str="Не указана"):
@@ -63,6 +65,53 @@ class AdminCog(commands.Cog):
         except Exception as e:
             print(e)
 
+    @app_commands.command(name="mute", description="Выдать мьют")
+    @app_commands.describe(member="Участник", duration="Длительность", reason="Причина")
+    @app_commands.checks.has_permissions(mute_members=True)
+    async def mute(self, interaction: discord.Interaction, member: discord.Member, duration: str = "10m", reason: str = "Не указана"):
+        try:
+            until = datetime.timedelta(minutes=10)
+
+            embed = discord.Embed(color=discord.Color.dark_teal())
+            embed.add_field(name="", value=f"Пользователь {member.mention} **замьючен**", inline=False)
+            embed.add_field(name="Причина", value=reason, inline=True)
+
+            if duration[-1] == "m":
+                until = datetime.timedelta(minutes=int(duration[:-1]))
+                word = plural(duration[:-1], ("минута", "минуты", "минут"))
+                embed.add_field(name="Срок наказания", value=f"{duration[:-1]} {word}", inline=True)
+            if duration[-1] == "h":
+                until = datetime.timedelta(hours=int(duration[:-1]))
+                word = plural(duration[:-1], ("час", "часа", "часов"))
+                embed.add_field(name="Срок наказания", value=f"{duration[:-1]} {word}", inline=True)
+            if duration[-1] == "d":
+                until = datetime.timedelta(days=int(duration[:-1]))
+                word = plural(duration[:-1], ("день", "дня", "дней"))
+                embed.add_field(name="Срок наказания", value=f"{duration[:-1]} {word}", inline=True)
+            else:
+                await interaction.response.send_message("Неверный формат времени", ephemeral=True)
+                return
+
+            embed.add_field(name="", value=f"Выдал: {interaction.user.mention}", inline=False)
+
+            await member.timeout(until, reason=reason)
+            await interaction.response.send_message(embed=embed)
+
+        except Exception as e:
+            print(e)
+
 
 async def setup(bot):
     await bot.add_cog(AdminCog(bot))
+
+
+def plural(n, forms):
+    n = abs(int(n)) % 100
+    n1 = n % 10
+    if 11 <= n <= 19:
+        return forms[2]
+    if 1 == n1:
+        return forms[0]
+    if 2 <= n1 <= 4:
+        return forms[1]
+    return forms[2]
--- cogs/utils.py
+++ cogs/utils.py
@@ -1,10 +1,35 @@
-import discord
 from discord import app_commands
 from discord.ext import commands
+from collections import deque
+import asyncio
+import discord
+import yt_dlp
+import discord.voice_client
+
+
 
 class UtilsCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.is_playing = {}
+        self.song_queues = {}
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member, before, after):
+        voice_client = member.guild.voice_client
+
+        if not voice_client or not voice_client.is_connected():
+            return
+
+        voice_channel_bot = voice_client.channel
+
+        if before.channel == voice_channel_bot and after.channel != voice_channel_bot:
+            remaining_members = [m for m in voice_channel_bot.members if not m.bot]
+
+            if len(remaining_members) == 0:
+                await voice_client.disconnect()
+
+                self.song_queues = {}
 
     @app_commands.command(name="poll", description="Создать опрос")
     @app_commands.describe(question="Тема опроса", option1="Вариант ответа", option2="Вариант ответа", option3="Вариант ответа", option4="Вариант ответа", option5="Вариант ответа")
@@ -32,5 +57,117 @@ class UtilsCog(commands.Cog):
         except Exception as e:
             print(e)
 
+    @commands.command(name="112")
+    async def emergency(self, ctx, member: discord.Member = None, reason: str = None):
+        if ctx.channel.id == 1367128425503916086:
+            if member is None:
+                await ctx.message.delete(delay=3)
+                msg = await ctx.send("Вы не указали участника, на которого хотите пожаловаться")
+                await msg.delete(delay=3)
+                return
+            if reason is None:
+                await ctx.message.delete(delay=3)
+                msg = await ctx.send("Вы не указали причину жалобы")
+                await msg.delete(delay=3)
+                return
+            if not ctx.message.attachments:
+                await ctx.message.delete(delay=3)
+                msg = await ctx.send("Вы не прикрепили доказательства (видео/скрин)")
+                await msg.delete(delay=3)
+                return
+
+            await ctx.send(f"🚨 {ctx.author.mention} сообщил о правонарушении.\n\n👤 Подозреваемый: {member.mention}\n\n📋 Статья: {reason}\n\n🚓 Экипаж уже в пути.")
+
+        else:
+            await ctx.message.delete(delay=3)
+            await ctx.send("Здесь нельзя использовать эту команду").delete(delay=3)
+
+
+    @commands.command(name="play")
+    async def play(self, ctx, url: str = None):
+        try:
+            if ctx.channel.id != 1367128266552377418:
+                await ctx.message.delete(delay=3)
+                msg = await ctx.send("Здесь нельзя использовать эту команду")
+                await msg.delete(delay=3)
+                return
+
+            if url is None:
+                await ctx.message.delete(delay=3)
+                msg = await ctx.send("Вы не указали ссылку")
+                await msg.delete(delay=3)
+                return
+
+            if not ctx.author.voice:
+                await ctx.message.delete(delay=3)
+                msg = await ctx.send("Вы не в голосовом канале")
+                await msg.delete(delay=3)
+                return
+
+            guild_id = ctx.guild.id
+            voice_channel = ctx.author.voice.channel
+
+            if is_yt_link(url):
+                audio_url, title = get_url(url)
+            else:
+                audio_url = url
+                title = None
+
+            if guild_id not in self.song_queues:
+                self.song_queues[guild_id] = deque()
+            self.song_queues[guild_id].append(audio_url)
+
+            voice_client = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
+            if not voice_client or not voice_client.is_connected():
+                voice_client = await voice_channel.connect()
+            elif voice_client.channel != voice_channel:
+                await voice_client.move_to(voice_channel)
+
+            await ctx.send(f"🎶 Добавлено в очередь: {url}")
+
+            if not self.is_playing.get(guild_id, False):
+                self.is_playing[guild_id] = True
+
+                while self.song_queues[guild_id]:
+                    current = self.song_queues[guild_id].popleft()
+                    voice_client.play(discord.FFmpegPCMAudio(current))
+                    await ctx.send(f"▶️ Сейчас играет: {title or url}")
+
+                    while voice_client.is_playing():
+                        await asyncio.sleep(1)
+
+                self.is_playing[guild_id] = False
+                await voice_client.disconnect()
+
+        except Exception as e:
+            print(f"[ERROR] {e}")
+
+            await ctx.send("⚠️ Ошибка воспроизведения")
+
+    @commands.command(name="stop")
+    async def stop(self, ctx):
+        voice_client = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
+        await voice_client.disconnect()
+
+    @commands.command(name="skip")
+    async def skip(self, ctx):
+        voice_client = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
+
+        guild_id = ctx.guild.id
+        current = self.song_queues[guild_id].popleft()
+        voice_client.stop()
+        voice_client.play(discord.FFmpegPCMAudio(current))
+
 async def setup(bot):
     await bot.add_cog(UtilsCog(bot))
+
+
+def get_url(url):
+    ydl_opts = {'format': 'bestaudio/best', 'quiet': True, 'noplaylist': True, 'extract_flat': False,}
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+        return info['url'], info.get('title', 'Неизвестное видео')
+
+def is_yt_link(url: str) -> bool:
+    return "youtube.com" in url or "youtu.be" in url
